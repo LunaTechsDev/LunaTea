bb5609e49008ce21327d60d713e9849766f4fd4b
Kino  Rose <kinoarose@gmail.com>
2020-08-29 13:18:08 -0400

    Add Sprite Battler public privates

--- src/rm/sprites/Sprite_Battler.hx
+++ src/rm/sprites/Sprite_Battler.hx
@@ -5,14 +5,32 @@ import rm.objects.Game_Battler;
 @:expose("Sprite_Battler")
 @:native("Sprite_Battler")
 extern class Sprite_Battler extends Sprite_Base {
+ @:native("_battler")
+ public var __battler: Game_Battler;
  private var _battler: Game_Battler;
+ @:native("_homeX")
+ public var __homeX: Int;
  private var _homeX: Int;
+ @:native("_homeY")
+ public var __homeY: Int;
  private var _homeY: Int;
+ @:native("_offsetX")
+ public var __offsetX: Int;
  private var _offsetX: Int;
+ @:native("_offsetY")
+ public var __offsetY: Int;
  private var _offsetY: Int;
+ @:native("_targetOffsetX")
+ public var __targetOffsetX: Int;
  private var _targetOffsetX: Int;
+ @:native("_targetOffsetY")
+ public var __targetOffsetY: Int;
  private var _targetOffsetY: Int;
+ @:native("_movementDuration")
+ public var __movementDuration: Int;
  private var _movementDuration: Int;
+ @:native("_selectionEffectCount")
+ public var __selectionEffectCount: Int;
  private var _selectionEffectCount: Int;
 
  public function initMembers(): Void;
