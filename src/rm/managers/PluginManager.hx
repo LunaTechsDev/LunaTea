8817a0b5dad0cc0c9a69bd2d36a701249891cfdd
Kino  Rose <kinoarose@gmail.com>
2020-09-19 09:43:33 -0400

    Add call command and register command for MZ

--- src/rm/managers/PluginManager.hx
+++ src/rm/managers/PluginManager.hx
@@ -1,5 +1,6 @@
 package rm.managers;
 
+import haxe.extern.Rest;
 import js.html.Event;
 import rm.types.RM;
 
@@ -39,49 +40,58 @@ extern class PluginManager {
 
  public static function onError(e: Event): Void;
 
+ #if !compileMV
  /**
-    * Regsters a new plugin command in MZ using a lambda expression
-    * The Variable of the lambda should be transformed into the appropriate
-    * typed def struct for your use case for your command.
+     * Regsters a new plugin command in MZ using a lambda expression
+     * The Variable of the lambda should be transformed into the appropriate
+     * typed def struct for your use case for your command.
   * Example Syntax For Commented Parameters
   *
-        *```js
-        //Should be in comment
+
+
 
     @command PluginCommandFunctionName
-   @text My Plugin Command
-   @desc Plugin Command Description Text
+    @text My Plugin Command
+    @desc Plugin Command Description Text
 
-   @arg Actors
-    @text Which Actors?
-   @type actor[]
-   @desc Select which Actor ID(s) to affect.
-   @default ["1"]
+    @arg Actors
+     @text Which Actors?
+    @type actor[]
+    @desc Select which Actor ID(s) to affect.
+    @default ["1"]
 
-   @arg State
-   @text State
-   @type state
-   @desc Which State to apply?
-   @default 1
+    @arg State
+    @text State
+    @type state
+    @desc Which State to apply?
+    @default 1
 
-    PluginManager.registerCommand("TestPlugin", "PluginCommandFunctionName", args => {
-     // Get Arguments
-     const actorIds = JSON.parse(args.Actors);
-     const stateId = args.State;
-
-     // Use the arguments
-     for (const actorId of actorIds) {
-        const actor = $gameParty.members().find(member => member.actorId() === Number(actorId));
-        if (actor) {
-         actor.addState(stateId);
-        }
-     }
-    });
-    * ```
     * @param pluginName
     * @param commandName
     * @param lambda
+    ```js
+
+     PluginManager.registerCommand("TestPlugin", "PluginCommandFunctionName", args => {
+      // Get Arguments
+      const actorIds = JSON.parse(args.Actors);
+      const stateId = args.State;
+
+      // Use the arguments
+      for (const actorId of actorIds) {
+         const actor = $gameParty.members().find(member => member.actorId() === Number(actorId));
+         if (actor) {
+          actor.addState(stateId);
+         }
+      }
+     });
+     ```
+     * ```js
+
   */
  public static function registerCommand(pluginName: String,
   commandName: String, lambda: (Any) -> Void): Void;
+
+ public static function callCommand(self: Any, pluginName: String,
+  commandName: String, args: Rest<Dynamic>): Void;
+ #end
 }
