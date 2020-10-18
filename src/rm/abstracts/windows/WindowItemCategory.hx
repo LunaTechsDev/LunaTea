eabbb8f1ecb67ae983af8c470f6f1f7ceb67c855
Kino  Rose <kinoarose@gmail.com>
2020-10-18 19:05:05 -0400

    Fix website pushing

--- src/rm/abstracts/windows/WindowItemCategory.hx
+++ src/rm/abstracts/windows/WindowItemCategory.hx
@@ -1,12 +1,19 @@
 package rm.abstracts.windows;
 
+import rm.core.Rectangle;
 import rm.windows.Window_ItemCategory;
 
 @:forward
 @:forwardStatics
 abstract WindowItemCategory(Window_ItemCategory) from Window_ItemCategory
  to Window_ItemCategory {
- public inline function new(x: Float, y: Float) {
+ public inline function new(x: Float, y: Float, ?width: Float,
+   ?height: Float) {
+  #if compileMV
   this = new Window_ItemCategory(x, y);
+  #else
+  var rect = new Rectangle(x, y, width, height);
+  this = new Window_ItemCategory(rect);
+  #end
  }
 }
