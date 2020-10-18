47efffb84e56f9f98a345cb1c835229b5cd91be7
Kino  Rose <kinoarose@gmail.com>
2020-10-18 15:17:03 -0400

    Add conditional compilation for Window_HorzCommand

--- src/rm/windows/Window_HorzCommand.hx
+++ src/rm/windows/Window_HorzCommand.hx
@@ -1,9 +1,15 @@
 package rm.windows;
 
+import rm.core.Rectangle;
+
 @:expose("Window_HorzCommand")
 @:native("Window_HorzCommand")
 @:build(macros.BuildMacroTools.buildDynamicFunctions())
 @:build(macros.BuildMacroTools.buildPublicPrivateFields())
 extern class Window_HorzCommand extends Window_Command {
+ #if compileMV
  public function new(x: Float, y: Float): Void;
+ #else
+ public function new(rect: Rectangle): Void;
+ #end
 }
